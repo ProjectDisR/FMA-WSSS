4d88fe106a286def68847a0a265ad390ff9376b3
ProjectDisR <30362911+ProjectDisR@users.noreply.github.com>
2024-01-30 23:27:29 +0800

    Update score_cw.py

--- src/libs/seeding/seed_anns/score_cw.py
+++ src/libs/seeding/seed_anns/score_cw.py
@@ -6,7 +6,7 @@
 @Time    : 2023/5/24 22:11
 @File    : score_cw.py
 @Software: PyCharm
-@Desc    : 
+@Desc    :
 """
 import torch
 
@@ -75,6 +75,8 @@ def gather_norm_bg_argmax(anns: SamAnns, cam: torch.Tensor, fg_cls: torch.Tensor
 
     # * 计算标注背景得分。
     anns_score = cat_bg_score_cuda(anns_fg_score, bg_method)  # (C+1, 1, S)
+    print(anns_fg_score.max(), anns_fg_score.min(), anns_fg_score.sum())
+
     if ret_seeded_anns:
         anns.add_item('score', anns_score[:, 0, :].T)
 
