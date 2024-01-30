a47af0765d4cc33e99de0f826031a8357e5104c5
ProjectDisR <30362911+ProjectDisR@users.noreply.github.com>
2024-01-30 23:56:19 +0800

    Update score_cw.py

--- src/libs/seeding/seed_anns/score_cw.py
+++ src/libs/seeding/seed_anns/score_cw.py
@@ -83,7 +83,7 @@ def gather_norm_bg_argmax(anns: SamAnns, cam: torch.Tensor, fg_cls: torch.Tensor
 
     # * 得到标注种子点。
     anns_seed = idx2seed_cuda(anns_max_idx, fg_cls)  # (1, S)
-    print(anns_seed)
+    print((anns_seed==0).sum())
     anns.add_item('seed', anns_seed[0].tolist())
 
     # * 以得分为标注置信度。
