4c426e4d0423ebc8f7b28b0048fa2dd34aac01c3
ProjectDisR <30362911+ProjectDisR@users.noreply.github.com>
2024-02-02 18:33:16 +0800

    Update run.py

--- src/tasks/anns_seed/run.py
+++ src/tasks/anns_seed/run.py
@@ -97,7 +97,36 @@ for norm_first, bg_method in product(cfg.seed.norm_firsts, cfg.seed.bg_methods):
         # ** CAM插值到原图大小。
         cam = resize_cam_cuda(cam, (ori_h, ori_w))
 
-        cam_dict = np.load(f'/home/vllcslinv100/repos/SemPLeS/semples3_0.02_0.05_2/voc/attn-patchrefine-npy-ms/{img_id}.npy', allow_pickle=True)
+        # cam_dict = np.load(f'/home/vllcslinv100/repos/SemPLeS/semples3_0.02_0.05_2/voc/attn-patchrefine-npy-ms/{img_id}.npy', allow_pickle=True)
+        # cam_dict = cam_dict.item()
+
+        # cam_ls = []
+        # fg_cls_ls = []
+
+        # for cls_, cam in cam_dict.items():
+
+        #     cam_ls.append(cam)
+        #     fg_cls_ls.append(cls_)
+
+        # fg_cls = np.array(fg_cls_ls).astype(np.uint8)
+        # cam = np.stack(cam_ls, axis=0)
+
+        # fg_cls = torch.from_numpy(fg_cls).cuda()
+        # cam = torch.from_numpy(cam).cuda()
+
+
+
+
+        coco_cls_ls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13,
+                       14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                       24, 25, 27, 28, 31, 32, 33, 34, 35, 36,
+                       37, 38, 39, 40, 41, 42, 43, 44, 46, 47,
+                       48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+                       58, 59, 60, 61, 62, 63, 64, 65, 67, 70,
+                       72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
+                       82, 84, 85, 86, 87, 88, 89, 90]
+
+        cam_dict = np.load(f'/workspace/SemPLeS/semples3_0.01_0.2/coco/attn-patchrefine-npy-ms/{img_id}.npy', allow_pickle=True)
         cam_dict = cam_dict.item()
 
         cam_ls = []
@@ -106,7 +135,7 @@ for norm_first, bg_method in product(cfg.seed.norm_firsts, cfg.seed.bg_methods):
         for cls_, cam in cam_dict.items():
 
             cam_ls.append(cam)
-            fg_cls_ls.append(cls_)
+            fg_cls_ls.append(coco_cls_ls.index(cls_+1))
 
         fg_cls = np.array(fg_cls_ls).astype(np.uint8)
         cam = np.stack(cam_ls, axis=0)
