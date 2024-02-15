4bb252b8ef56303dce87b2c67cb200dddf0ce421
ProjectDisR <30362911+ProjectDisR@users.noreply.github.com>
2024-02-16 01:42:50 +0800

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -7,6 +7,11 @@ import numpy as np
 
 
 
+from alchemy_cat.data.plugins import arr2PIL
+
+
+
+
 
 
 
@@ -15,54 +20,33 @@ a = 1.0
 
 
 
-os.makedirs('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC_ensemble/infer/best,ss/seg_preds/')
+os.makedirs('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC_ensemble/infer/best,ss/masks/')
 
 
 
-npz_name_ls = os.listdir('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC/infer/best,ss/seg_preds/')
+npz_name_ls = os.listdir('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC_fma/infer/best,ss/seg_preds/')
 
 for npz_name in npz_name_ls:
 
-    fma_npz = np.load(os.path.join('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC/infer/best,ss/seg_preds/', npz_name))
+    fma_npz = np.load(os.path.join('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC_fma/infer/best,ss/seg_preds/', npz_name))
     semples_npz = np.load(os.path.join('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC_semples/infer/best,ss/seg_preds/', npz_name))
 
 
 
-    key_ls = []
-    prob_ls = []
-
-    key_ls.append(0)
-    prob_ls.append(fma_npz['prob'][0]*a + semples_npz['prob'][0]*(1.0-a))
+    mask = np.zeros((21, fma_npz['prob'][0].shape[0], fma_npz['prob'][0].shape[1]))
+    mask[0] = mask[0] + a*fma_npz['prob'][0] + (1.0-a)*semples_npz['prob'][0]
 
 
-    for key in range(1, 21):
+    for cls_ in range(1, 21):
 
-        fma_prob = None
+        if cls_ in fma_npz['fg_cls']+1:
+            fma_prob = fma_npz['prob'][np.where(fma_npz['fg_cls']+1==cls_)[0][0]+1]
+            mask[cls_] = mask[cls_] + a*fma_prob
 
-        if key in fma_npz['fg_cls']+1:
-            fma_prob = fma_npz['prob'][np.where(fma_npz['fg_cls']+1==key)[0][0]+1]
-        else:
-            fma_prob = np.zeros_like(fma_npz['prob'][0])
-
-
-
-        semples_prob = None
-
-        if key in semples_npz['fg_cls']+1:
-            semples_prob = semples_npz['prob'][np.where(semples_npz['fg_cls']+1==key)[0][0]+1]
-        else:
-            semples_prob = np.zeros_like(semples_npz['prob'][0])
+        if cls_ in semples_npz['fg_cls']+1:
+            semples_prob = semples_npz['prob'][np.where(semples_npz['fg_cls']+1==cls_)[0][0]+1]
+            mask[cls_] = mask[cls_] + (1.0-a)*semples_prob
 
 
-
-
-        if fma_prob is None and semples_prob is None:
-            continue
-        else:
-            key_ls.append(key)
-            prob_ls.append(fma_prob*a + semples_prob*(1.0-a))
-
-    keys = np.array(key_ls)
-    prob = np.stack(prob_ls, axis=0)
-
-    np.save(os.path.join('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC_ensemble/infer/best,ss/seg_preds/', npz_name.split('.')[0]+'.npy'), {"prob": prob, "keys": keys})
+    mask = mask.argmax(dim=0).astype(np.uint8)
+    arr2PIL(mask).save(os.path.join('experiment/others/mmseg/m2f-sl22-bt4-80k-512x-VOC_ensemble/infer/best,ss/masks/', npz_name.split('.')[0]+'.png'))
