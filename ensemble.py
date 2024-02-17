d69400d36e1c95d96954e84922a4b7d50c02859d
ProjectDisR <30362911+ProjectDisR@users.noreply.github.com>
2024-02-17 23:06:23 +0800

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -120,15 +120,18 @@ for npz_name in npz_name_ls:
 
 
     for cls_ in range(1, 81):
+        
+        print(fma_npz['fg_cls'])
+        print(fma_npz['prob'].shape)
 
         if cls_ in fma_npz['fg_cls']+1:
             fma_prob = fma_npz['prob'][np.where(fma_npz['fg_cls']+1==cls_)[0][0]+1]
             mask[cls_] = mask[cls_] + a*fma_prob
 
-        if cls_ in semples_npz['fg_cls']+1:
-            semples_prob = semples_npz['prob'][np.where(semples_npz['fg_cls']+1==cls_)[0][0]+1]
-            mask[cls_] = mask[cls_] + (1.0-a)*semples_prob
+        # if cls_ in semples_npz['fg_cls']+1:
+        #     semples_prob = semples_npz['prob'][np.where(semples_npz['fg_cls']+1==cls_)[0][0]+1]
+        #     mask[cls_] = mask[cls_] + (1.0-a)*semples_prob
 
 
-    mask = mask.argmax(axis=0).astype(np.uint8)
-    arr2PIL(mask).save(os.path.join('experiment/others/mmseg/m2f-sl22-bt4-100k-512x-COCO_ensemble/infer/best,ss/masks/', npz_name.split('.')[0]+'.png'))
+    # mask = mask.argmax(axis=0).astype(np.uint8)
+    # arr2PIL(mask).save(os.path.join('experiment/others/mmseg/m2f-sl22-bt4-100k-512x-COCO_ensemble/infer/best,ss/masks/', npz_name.split('.')[0]+'.png'))
